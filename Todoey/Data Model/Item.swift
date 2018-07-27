89857b439ad9107e9d019907b554257929adc55b
iMac 2012 <imac2012@imac-imac.local>
2018-07-27 21:45:01 +0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by iMac 2012 on 27.07.2018.
+//  Copyright © 2018 iMac2012. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
