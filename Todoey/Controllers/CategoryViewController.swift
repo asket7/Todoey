24da745dd209fb241628cc0e4cabb1f90a913c11
iMac 2012 <imac2012@imac-imac.local>
2018-07-28 23:25:59 +0500

    Prepare for challenge "Create Database Categories Items"

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by iMac 2012 on 28.07.2018.
+//  Copyright © 2018 iMac2012. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //MARK: - TableView Datasourse Methods
+    
+    
+    //MARK: - Data Manitulation Methods
+    
+    
+    //MARK: - Add New Categories
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+
+    
+}
